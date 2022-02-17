f697b304248e802ead66bbee9b11e5b5016b563e
GodLuciferXD <98580170+GodLuciferXD@users.noreply.github.com>
2022-02-17 18:07:35 +0530

    Update qnrotus.py

--- qnrotus.py
+++ qnrotus.py
@@ -1,23 +1,19 @@
 import os
 import sys
-import git
 import random
-import heroku3
 from datetime import datetime
-from​ .. ​import str1, ​DEV​, ​HEROKU_APP_NAME​, ​HEROKU_API_KEY
-from .. import CMD_HNDLR as hl
 from os import execl
-from telethon import TelegramClient, events
+from telethon import TelegramClient, event
 from telethon.sessions import StringSession
 from telethon.tl.functions.account import UpdateProfileRequest
 from Config import STRING, SUDO, BIO_MESSAGE, API_ID, API_HASH, STRING2, STRING3, STRING4 ,STRING5, STRING6, STRING7, STRING8 ,STRING9, STRING10, STRING11, STRING12, STRING13, STRING14, STRING15, STRING16, STRING17, STRING18, STRING19, STRING20, STRING21, STRING22, STRING23, STRING24, STRING25, STRING26, STRING27, STRING28, STRING29, STRING30
 import asyncio
 import telethon.utils
-from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl import functions
 from telethon.tl.functions.channels import LeaveChannelRequest
 from telethon.tl.functions.messages import ImportChatInviteRequest
 from Utils import RAID, RRAID
+
 
 
 a = API_ID
